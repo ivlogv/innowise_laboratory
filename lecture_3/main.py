307d620e7bb2377ea892463958fa3ccbdd32a47f
Ivan Logvinov <ivan.logvinov.10@gmail.com>
2025-11-20 18:22:00 +0300

    Add function for adding new student to the list.

--- lecture_3/main.py
+++ lecture_3/main.py
@@ -1,23 +1,69 @@
 """This module contains Student Grade Analyzer program"""
 
-students = [
-    {"name": None, "grades": []}
-]
+students = [] # Empty list to store students data
+
+
+def check_student_exist(name: str):
+    """
+    Check student's name in students list
+
+    Args:
+        name (str): Student name
+
+    Returns:
+        bool: Student existance    
+    """
+    if any(student["name"] == name for student in students.copy()):
+        print("Student already exists.")
+        return True
+    return False
+
+
+def update_student_grades(name: str, grade: int):
+    """
+    Add new grade to a student's grades by given name.
+    
+    Args:
+        name (str): Name of a student
+        grade (int): Grade to add to list
+    """
+    for student in students:
+        if student["name"] == name:
+            student["grades"].append(grade)
 
 
 def add_student():
     """Add new user to dictionary"""
+    name = str(input("Enter studens name: "))
+    if not check_student_exist(name):
+        new_student = {
+            "name": name,
+            "grades": []
+        }
+        students.append(new_student)
 
 
-def add_grades(student_name: str):
+def add_grades():
     """
     Add grades list for specific student.
     Input numbers 0-100
-
-    Args:
-        student_name (str): Name of a student
     """
-    return student_name
+    name = str(input("Enter studens name: "))
+    if check_student_exist(name):
+        # ask for grades
+        while True:
+            grade_str = input("Enter a grade (or 'done' to finish):")
+            if grade_str.lower() == "done":
+                break
+            try:
+                grade = int(grade_str)
+                if 0 <= grade <=100:
+                    update_student_grades(name, grade)
+                else:
+                    print("Invalid input. Please enter a number (0-100).")
+            except ValueError:
+                print("Invalid input. Please enter a number (0-100).")
+
 
 
 def show_report():
@@ -39,6 +85,7 @@ def main():
         print("4. Find top performer")
         print("5. Exit")
         print("-" * 30)
+        print(students)
 
         # Handle potential input errors
         try:
@@ -51,7 +98,7 @@ def main():
                     add_student()
                 case 2:
                     print("add grades for a student")
-                    add_grades("test")
+                    add_grades()
                 case 3:
                     print("show report")
                     show_report()
@@ -62,7 +109,7 @@ def main():
                     print("Exiting program.")
                     break
                 case _:
-                    print("Invalid input. Please enter a number 1-5.")
+                    print("Invalid input. Please enter a correct number (1-5).")
         # Handle not numeric input
         except ValueError:
             print("Invalid input. Please enter a number.")
