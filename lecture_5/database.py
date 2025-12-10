9345efaa6e28e0794281526d1682dfcba134f11f
Ivan Logvinov <ivan.logvinov.10@gmail.com>
2025-12-10 19:40:20 +0300

    Add lecture_5 base structure and database connection via sqlalchemy

---
+++ lecture_5/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+DATABASE_URL = "sqlite+aiosqlite:///./books.db"
+
+
+engine = create_async_engine(url=DATABASE_URL, echo=True)
+SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
---
+++ lecture_5/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from .database import Base
+
+
+class Books(Base):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    author: Mapped[str] = mapped_column(nullable=False)
+    year: Mapped[int] = mapped_column(nullable=True)
