854abc92e43633778e82c7a227fd7e5ae3db5a62
Ivan Logvinov <ivan.logvinov.10@gmail.com>
2025-12-10 22:10:22 +0300

    Add Annotated and Path parameters to session and book_id in required endpoints

--- lecture_5/main.py
+++ lecture_5/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Depends, Query
+from fastapi import FastAPI, Depends, Query, Path
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Annotated
 
 from .database import SessionLocal, init_db
 from . import crud, schemas
@@ -21,33 +21,39 @@ async def get_db() -> AsyncGenerator[AsyncSession, None]:
         yield session
 
 
+DbSession = Annotated[AsyncSession, Depends(get_db)]
+
+
 @app.get("/")
 async def root():
     return {"status": "ok"}
 
 
 @app.get("/books/", response_model=list[schemas.BookRead])
-async def read_books(db: AsyncSession = Depends(get_db)):
+async def read_books(db: DbSession):
     return await crud.get_books(db)
 
 
 @app.get("/books/{book_id}", response_model=schemas.BookRead)
-async def read_book(book_id: int, db: AsyncSession = Depends(get_db)):
+async def read_book(
+    book_id: Annotated[int, Path(gt=0, description="Book ID to retrieve >0")],
+    db: DbSession
+):
     return await crud.get_book(db, book_id)
 
 
 @app.post("/books/", response_model=schemas.BookCreate)
 async def create_book(
     book: schemas.BookCreate,
-    db: AsyncSession = Depends(get_db)
+    db: DbSession
 ):
     return await crud.create_book(db, book)
 
 
 @app.delete("/books/{book_id}", response_model=schemas.BookDeleteResponse)
 async def delete_book(
-    book_id: int,
-    db: AsyncSession = Depends(get_db)
+    book_id: Annotated[int, Path(gt=0, description="Book ID to delete >0")],
+    db: DbSession
 ) -> schemas.BookDeleteResponse:
     deleted_book = await crud.delete_book(db, book_id)
     return schemas.BookDeleteResponse(
@@ -58,15 +64,16 @@ async def delete_book(
 
 @app.put("/books/{book_id}", response_model=schemas.BookRead)
 async def update_book(
-    book_id: int,
+    book_id: Annotated[int, Path(gt=0, description="Book ID to update >0")],
     book: schemas.BookUpdate,
-    db: AsyncSession = Depends(get_db)
+    db: DbSession
 ):
     return await crud.update_book(db, book_id, book)
 
 
 @app.get("/books/search/", response_model=list[schemas.BookRead])
 async def search_books(
+    db: DbSession,
     title: str | None = Query(
         None,
         min_length=1,
@@ -78,6 +85,5 @@ async def search_books(
         max_length=50
     ),
     year: int | None = Query(None, ge=0),
-    db: AsyncSession = Depends(get_db)
 ):
     return await crud.search_books(db, title=title, author=author, year=year)
